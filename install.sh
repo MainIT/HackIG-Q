2be8cd2eb8fddb2341f8564a41aa937404a057f1
MainIT <67462191+MainIT@users.noreply.github.com>
2020-08-23 21:52:42 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+clear
+echo "MengInstall..."
+clear
+sleep 1
+echo "MengInstall..."
+clear
+sleep 1
+echo "MengInstall..."
+clear
+sleep 1
+echo "MengInstall..."
+clear
+sleep 1
+echo "MengInstall..."
+clear
+sleep 1
+clear
+pkg update && pkg upgrade
+pkg install python2
+pip2 install requests
+pip2 install bs4
